f7cb214e27fed9036c86a482e2b183ccfed9d796
yongnology <cassius20@naver.com>
2024-12-02 00:26:40 +0900

    3.2 뷰 애플리케이션 인스턴스

--- 01/src/main.js
+++ 01/src/main.js
@@ -1,6 +1,24 @@
-import './assets/main.css'
+// import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp, h } from 'vue'
+// import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+createApp({
+    data() {
+        return {
+            message: 'hello, Vue!',
+        };
+    },
+    method: {
+        reverse() {
+            this.message = this.message.split('').reverse().join('');
+        },
+    },
+    render() {  // data와 methods에 정의한  속성 사용
+        return h('div', [
+            h('p', this.message),
+            h('button', { onclick: this.reverse }, 'Reverse'),
+        ]);
+    },
+}).mount('#app');
