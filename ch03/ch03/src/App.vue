28861462a998aa991c9f2439d5191e4fbdb11593
yongnology <cassius20@naver.com>
2024-12-02 23:50:32 +0900

    3.3.5 디렉티브 사용하기_v-html, v-text, v-pre, v-bind

---
+++ ch03/ch03/src/App.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+      message:'<h1>hello, Vue JS! ch03</h1>',
+      message2:'<h1>hello, Vue JS! ch03</h1>',
+      message3:'<h1>hello, Vue JS! ch03</h1>',
+      className: 'red-color',
+      className2: 'blue-color',
+      class: 'title',
+
+    };
+  },
+}
+</script>
+
+<template>
+  <!-- <div v-html="message"></div> -->
+  <!-- <div v-text="message2"></div> -->
+  <!-- <div v-pre>{{ message3 }}</div> -->
+   <h1 v-bind:class="className">Hello, Vue JS !!!</h1>
+   <h2 :class="className2">Hello, Vue</h2> <!--축약형-->
+   <h3 :class>Hello, Vue 3.4 upper JS</h3>
+</template>
+
+<style>
+  .red-color {
+    color: red;
+  }
+  .blue-color {
+    color: blue;
+  }
+  .title {
+    color: brown;
+  }
+</style>
