3930b050bac4409ecd09b220018b2897393ab6b4
yongnology <cassius20@naver.com>
2024-12-03 23:44:34 +0900

    v-if

--- ch03/ch03/src/App.vue
+++ ch03/ch03/src/App.vue
@@ -9,6 +9,8 @@ export default {
       className2: 'blue-color',
       class: 'title',
 
+      visible: true,
+      unvisible: false,
     };
   },
 }
@@ -18,9 +20,11 @@ export default {
   <!-- <div v-html="message"></div> -->
   <!-- <div v-text="message2"></div> -->
   <!-- <div v-pre>{{ message3 }}</div> -->
-   <h1 v-bind:class="className">Hello, Vue JS !!!</h1>
-   <h2 :class="className2">Hello, Vue</h2> <!--축약형-->
-   <h3 :class>Hello, Vue 3.4 upper JS</h3>
+   <!-- <h1 v-bind:class="className">Hello, Vue JS !!!</h1> -->
+   <!-- <h2 :class="className2">Hello, Vue</h2> 축약형 -->
+   <!-- <h3 :class>Hello, Vue 3.4 upper JS</h3> -->
+    <p v-if="visible">이 요소는 렌더링됩니다.</p>
+    <p v-if="unvisible">이 요소는 렌더링 되지 않습니다.</p>
 </template>
 
 <style>
